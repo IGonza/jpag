54fbc884d83073831095a4d44989f0a4b328ca6f
IGonza <IGonza@192.168.1.112>
2013-01-12 00:38:16 -0500

    removed nbprojects file from the project

--- index.php
+++ index.php
@@ -4,18 +4,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-echo "aaa";
 
-
-
-echo "a";
-echo "b";
-$x = 1;
-
-
-
-$x = 3;
-$x = 4;
-echo $x;
 
 ?>
