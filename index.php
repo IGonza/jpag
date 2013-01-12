f86c9e3f7ffe24c5d42fdfb2132af0fcacf34a05
marcsteeleMAC <marcsteeleMAC@192.168.1.113>
2013-01-12 12:16:27 -0700

    more changes, for learning

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-// changes from Marc
+// 1. I make a change in the code (Local Repository)
+// 2. I save to my Local = Commit
+// 3. Push from local repositiry to remote
+// 
+//
 
 ?>
