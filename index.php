6425a2abe8e71410f053b2fad8771b88c28265a5
IGonza <IGonza@192.168.1.112>
2013-01-12 18:09:32 -0500

    created test1 function

--- index.php
+++ index.php
@@ -1,31 +1,8 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This file is a demo page with jpag
+ * 
  */
-// 1. I make a change in the code (Local Repository)
-// 2. I save to my Local = Commit
-// 3. Push from local repositiry to remote
-// 
-//test 2
-// what if we push the changes in not the latest file 
-
-// test changes from Igor
-function test1($x) {
-    
-    return $x;
-    
-}
-
-// new changes before fetch
-
-
-// function by Marc
-function test2($y) {
-    
-    return $y;
-    
-}
 
 ?>
